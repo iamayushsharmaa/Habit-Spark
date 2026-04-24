bb114ef4382161535c459a71babbeca73ff7a275
iamayushsharmaa <ayushsharmagit@gmail.com>
2026-04-25 00:19:42 +0530

    feat: add work manager to habitapp class

--- app/src/main/java/com/example/habittracker/view/navigation/App.kt
+++ app/src/main/java/com/example/habittracker/view/navigation/App.kt
@@ -1,9 +1,18 @@
 package com.example.habittracker.view.navigation
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App: Application(){
+class HabitApp : Application(), Configuration.Provider {
 
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
